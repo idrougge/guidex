108863b2607481a700fe952514c5b20922a2ae4d
Iggy Drougge <iggy@Iggys-MacBook-Pro.local>
2017-10-10 21:32:59 +0200

    Initial Commit

---
+++ AmigaGuideX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AmigaGuideX
+//
+//  Created by Iggy Drougge on 2017-10-10.
+//  Copyright © 2017 Iggy Drougge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AmigaGuideX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AmigaGuideX
+//
+//  Created by Iggy Drougge on 2017-10-10.
+//  Copyright © 2017 Iggy Drougge. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
