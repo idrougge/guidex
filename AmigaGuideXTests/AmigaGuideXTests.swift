9d18c54ecc9a8f3ebfecea1fcf697e7b8ba944c1
Iggy Drougge <idrougge@mac.com>
2018-02-27 08:37:03 +0100

    Test cases for later

--- AmigaGuideXTests/AmigaGuideXTests.swift
+++ AmigaGuideXTests/AmigaGuideXTests.swift
@@ -24,6 +24,25 @@ class AmigaGuideXTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        /*
+ @{ " Amiga E " link amiga-e }
+ @{ " Aztec C " link manx-c }
+ @{ " DICE C " link dice-c }
+ @{ " GNU C/C++ " link gnu-cpp}
+ @{ " SAS C 6.3 " link sas-c }
+ @{ " Maxon C++ V1.2.1 " link maxon-cpp }
+ @{ " Assemblers " link assemblers }
+ @{ " Other systems... " link other }
+ (nod l-systeme i AProf.guide)
+         @{" Assignment and Copying " Link "Assignment and Copying" }
+         @{" Pointers and Memory Allocation " Link "Pointers and Memory Allocation" }
+         @{" String and List Misuse " Link "String and List Misuse" }
+         @{" Initialising Data " Link "Initialising Data" }
+         @{" Freeing Resources " Link "Freeing Resources" }
+         @{" Pointers and Dereferencing " Link "Pointers and Dereferencing" }
+         @{" Mathematics Functions " Link "Mathematics Functions" }
+         @{" Signed and Unsigned Values " Link "Signed and Unsigned Values" }
+ */
     }
     
     func testPerformanceExample() {
